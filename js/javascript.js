9c84e26e6ef08abf4bdc354eb6f6649365c98df3
Alisa <alisa.luoma@hotmail.fi>
2023-01-30 13:28:05 +0200

    Challenge comleted

---
+++ js/javascript.js
@@ -0,0 +1,16 @@
+/* 
+ * 
+ *  Name: Alisa Luomanmäki
+ * 
+ */
+
+
+let width = screen.availWidth;
+
+if(width >= 1440)
+{
+    document.getElementById("qr").style.width = "300px";
+} else {
+    document.getElementById("qr").style.width = "200px";
+
+}
